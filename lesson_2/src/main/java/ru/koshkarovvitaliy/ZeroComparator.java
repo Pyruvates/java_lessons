b31a4d52e9eb64b151758c3287d639dee7b137f7
Vitaliy Koshkarov <redrick.shuhart123@gmail.com>
2022-03-20 13:32:12 +0300

    Lesson 2

---
+++ lesson_2/src/main/java/ru/koshkarovvitaliy/ZeroComparator.java
@@ -0,0 +1,33 @@
+package ru.koshkarovvitaliy;
+
+import java.util.Scanner;
+
+/*
+    Написать программу, которая считывает с консоли целое число,
+    сравнивает его с нулём и выводит результат сравнения в консоль
+ */
+
+public class ZeroComparator {
+    public static void main(String[] args) {
+        System.out.println("Start");
+        int number;
+
+        try (Scanner scanner = new Scanner(System.in)) {
+
+            System.out.println("Enter any number");
+
+            if (scanner.hasNext()) {
+                number = scanner.nextInt(10);
+            }
+//            Написать свой код тут
+            if (number) {
+
+            }
+
+            System.out.println("End");
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+}
---
+++ lesson_3/src/main/java/ru/koshkarovvitaliy/GuessTheNumberGame.java
@@ -0,0 +1,34 @@
+package ru.koshkarovvitaliy;
+
+import java.util.Random;
+import java.util.Scanner;
+
+/*
+    Написать игру, в которой предлагается угадать число от 1 до 10.
+    Игрок вводит целое число, а программа сравнивает его и выводит результат сравнения в консоль.
+    Игроку даётся 3 попытки. В случае угадывания в консоль выводится 'Верно!', в случае неудачной попытки подсказка -
+    'Больше' или 'Меньше' и предлагается угадать ещё раз. В случае если игрок провалил все 3 попытки, игра заканчивается
+    и в консоль должно быть выведено загаданное число.
+ */
+
+public class GuessTheNumberGame {
+    public static void main(String[] args) {
+        System.out.println("Start");
+        String guessStr = "Guess the number from 1 to 10";
+        int num = new Random().nextInt(1, 11);
+        int inputNumber;
+        System.out.println(guessStr);
+
+        try (Scanner scanner = new Scanner(System.in)) {
+
+            if (scanner.hasNext()) {
+                inputNumber = scanner.nextInt(10);
+//              Написать свой код тут
+            }
+
+            System.out.println("End");
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+}

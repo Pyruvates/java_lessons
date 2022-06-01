5006743fa383309d5a52197ffdbc2cf5a6d1dbc7
Nikrogi <famgdni@gmail.com>
2022-06-01 13:48:42 +0300

    little change in Task5.java

--- chapter_2/src/main/java/ru/koshkarovvitaliy/Task5.java
+++ chapter_2/src/main/java/ru/koshkarovvitaliy/Task5.java
@@ -1,8 +1,8 @@
 package ru.koshkarovvitaliy;
 public class Task5 {
+    int inputNumber = 50;
     public static void main(String[] args) {
     }
-        int inputNumber = 50;
         public void primeNumbers (int inputNumber) {
             while (inputNumber > 1) {
                 if (inputNumber % 2 != 0 && inputNumber % 3 != 0 && inputNumber % 5 != 0 && inputNumber % 7 != 0) {

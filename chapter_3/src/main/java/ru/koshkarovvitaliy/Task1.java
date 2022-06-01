d89d122afaa4d735d9360781ad1f40365fec6c56
Nikrogi <famgdni@gmail.com>
2022-06-01 16:54:46 +0300

    Add Task1.java in chapter_3

--- chapter_3/src/main/java/ru/koshkarovvitaliy/Task1.java
+++ chapter_3/src/main/java/ru/koshkarovvitaliy/Task1.java
@@ -1,2 +1,19 @@
-package ru.koshkarovvitaliy;public class Task1 {
+package ru.koshkarovvitaliy;
+
+public class Task1 {
+    public static void main(String[] args) {
+        int sizeFirst = 20;
+        int sizeSecond = 10;
+        int repeat = 1;
+        while (sizeFirst >= 1) {
+            String treugolnik = "*".repeat(sizeFirst);
+            System.out.println(treugolnik);
+            --sizeFirst;
+        }
+        while (sizeSecond > repeat) {
+            String treugolnik = "*".repeat(sizeSecond - (sizeSecond - repeat));
+            System.out.println(treugolnik);
+            ++repeat;
+        }
+    }
 }
